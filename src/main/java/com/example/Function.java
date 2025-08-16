d110d4c311957508c21d96c3e0b82b7f1b3f3847
Shariff Navab <shariff.navab@opstree.com>
2025-08-16 15:13:58 +0530

    adding source code

---
+++ src/main/java/com/example/Function.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import com.microsoft.azure.functions.*;
+import com.microsoft.azure.functions.annotation.*;
+
+public class Function {
+    @FunctionName("HttpTrigger-Java")
+    public HttpResponseMessage run(
+            @HttpTrigger(
+                name = "req",
+                methods = {HttpMethod.GET, HttpMethod.POST},
+                authLevel = AuthorizationLevel.ANONYMOUS) 
+                HttpRequestMessage<String> request,
+            final ExecutionContext context) {
+        
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        return request.createResponseBuilder(HttpStatus.OK)
+                .body("Hello from Azure Functions!")
+                .build();
+    }
+}
